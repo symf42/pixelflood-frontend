12d8952dffafd55d28df65b85316cdc77e2ee2c3
Rubeus Swagrid <112153643+symf42@users.noreply.github.com>
2022-11-16 20:33:16 +0000

    feat: added vite vue tailwindcss flowbite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
